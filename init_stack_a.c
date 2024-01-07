36462aff8d1ce786adc72569663a089a4f046a21
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-07 15:56:28 -0300

    Update

---
+++ init_stack_a.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_stack_a.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/07 15:49:20 by yufonten          #+#    #+#             */
+/*   Updated: 2024/01/07 15:56:19 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static long	ft_atol(const char *str)
+{
+	int		i;
+	int		sign;
+	long	n;
+
+	i = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == 43 || str[i] == 45)
+	{
+		if (str[i] == 45)
+			sign *= -1;
+		i++;
+	}
+	while (ft_isdigit(str[i]))
+	{
+		n = (n * 10) + (str[i] - 48);
+		i++;
+	}
+	return (n * sign);
+}
+
+
