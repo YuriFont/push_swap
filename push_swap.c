423449ba289e302bd8fa08250dde37842e0677b6
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-07 14:47:33 -0300

    Update

--- push_swap.c
+++ push_swap.c
@@ -1,29 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/06 14:32:24 by yufonten          #+#    #+#             */
-/*   Updated: 2024/01/07 14:24:34 by yufonten         ###   ########.fr       */
+/*   Created: 2024/01/07 14:29:52 by yufonten          #+#    #+#             */
+/*   Updated: 2024/01/07 14:47:24 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include "libft/libft.h"
-# include "ft_printf/ft_printf.h"
-
-typedef struct s_snode
+int	main(int ac, char **av)
 {
-	int				data;
-	int				index;
-	int				push_cost;
-	struct s_snode	*target_node;
-	struct s_snode	*next;
-	struct s_snode	*prev;
-}	t_snode;
-
-#endif
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		return (1);
+	else if (ac == 2)
+		av = ft_split(av[1], ' ');
+	else
+		av++;
+	return (0);
+}
