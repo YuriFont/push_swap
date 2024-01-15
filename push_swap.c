aba8514d420a176fb861bbba630f0542ce87be73
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-15 15:28:07 -0300

    Update

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:29:52 by yufonten          #+#    #+#             */
-/*   Updated: 2024/01/07 15:01:32 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/01/15 15:27:32 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,25 @@
 
 int	main(int ac, char **av)
 {
+	t_snode	*stack_a;
+	t_snode	*stack_b;
+
 	if (ac == 1 || (ac == 2 && !av[1][0]))
 		return (1);
 	else if (ac == 2)
 		av = ft_split(av[1], ' ');
 	else
 		av++;
-	ft_printf("%s", av[0]);
+	init_stack(&a, av);
+	if (!sorted_stack(a))
+	{
+		if (stack_length(a) == 2)
+			sa(&a);
+		else if (stack_length(a) == 3)
+			sorted_three(&a);
+		else
+			sort_stack(&a);
+	}
+	free_stack(&a);
 	return (0);
 }
