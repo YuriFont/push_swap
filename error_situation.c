def0bf7a0c62bfc4536f5119ca03fdbaa984023c
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-23 02:07:05 -0300

    Update

---
+++ error_situation.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_situation.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/23 02:05:39 by yufonten          #+#    #+#             */
+/*   Updated: 2024/01/23 02:06:39 by yufonten         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	free_stack(t_snode **head)
+{
+	t_snode	*tmp;
+	t_snode	*release;
+
+	if (!head == NULL)
+		return ;
+	release = *head;
+	while (release)
+	{
+		tmp = release->next;
+		free(release);
+		release = tmp;
+	}
+	*head = NULL;
+}
+
+void	free_matrix(char **av)
+{
+	int	i;
+
+	i = 0;
+	if (av == NULL || *av == NULL)
+		return ;
+	while (av[i])
+	{
+		free(av[i]);
+		i++;
+	}
+	free(av);
+}
