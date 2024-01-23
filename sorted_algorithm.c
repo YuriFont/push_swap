2897c8b6d45ac00b1c7879d26d0b94b287c3f022
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-23 15:19:54 -0300

    Update

--- sorted_algorithm.c
+++ sorted_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:16:11 by yufonten          #+#    #+#             */
-/*   Updated: 2024/01/23 15:08:31 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:18:09 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,13 @@ void    sorted_algorithm(t_snode **stack_a, t_snode **stack_b)
 	{
 		tmp1 = *stack_a;
 		min = find_min_node(*stack_a);
-		while (tmp1 != min && tmp1 != NULL)
+		while (tmp1 != min && tmp1 != NULL && stack_length(*stack_a) != 1)
 		{
 			tmp1 = tmp1->next;
 			ra(stack_a);
 		}
-		pb(stack_b, stack_a);
+		if (stack_length(*stack_a) != 0)
+			pb(stack_b, stack_a);
 	}
 	tmp2 = *stack_b;
 	while (tmp2 != NULL)
