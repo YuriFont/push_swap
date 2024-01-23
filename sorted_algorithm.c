5eb8f57a58047ef25e4915a59b5f0e2fed78cf6e
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-23 15:23:43 -0300

    Update

--- sorted_algorithm.c
+++ sorted_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: yufonten <yufonten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 14:16:11 by yufonten          #+#    #+#             */
-/*   Updated: 2024/01/23 15:18:09 by yufonten         ###   ########.fr       */
+/*   Updated: 2024/01/23 15:23:03 by yufonten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,25 @@
 void    sorted_algorithm(t_snode **stack_a, t_snode **stack_b)
 {
 	t_snode	*min;
-	t_snode	*tmp1;
-	t_snode	*tmp2;
+	t_snode	*tmp;
 
-	tmp1 = *stack_a;
-	while (tmp1 != NULL)
+	tmp = *stack_a;
+	while (tmp != NULL)
 	{
-		tmp1 = *stack_a;
+		tmp = *stack_a;
 		min = find_min_node(*stack_a);
-		while (tmp1 != min && tmp1 != NULL && stack_length(*stack_a) != 1)
+		while (tmp != min && tmp != NULL && stack_length(*stack_a) != 1)
 		{
-			tmp1 = tmp1->next;
+			tmp = tmp->next;
 			ra(stack_a);
 		}
 		if (stack_length(*stack_a) != 0)
 			pb(stack_b, stack_a);
 	}
-	tmp2 = *stack_b;
-	while (tmp2 != NULL)
+	tmp = *stack_b;
+	while (tmp != NULL)
 	{
-		tmp2 = tmp2->next;
+		tmp = tmp->next;
 		pa(stack_a, stack_b);
 	}
 }
